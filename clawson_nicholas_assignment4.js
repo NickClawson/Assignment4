e7938b955ae418aaee4ffbf15d51a1db36aa2e66
Nick Clawson <NickClawson@fullsail.edu>
2012-05-23 17:42:11 -0400

    created checkEmail function
    added checkEmail function to library file which verifies that a given
    email address follows the aaa@bbb.ccc formula, not restricting aaa and
    bbb to only 3 characters. added test to main assignment file.

--- clawson_nicholas_assignment4.js
+++ clawson_nicholas_assignment4.js
@@ -1,7 +1,10 @@
 var phone = "440-506-8107";
+var email = "NickClawson@fullsail.edu"
 var lib = nixLib();
 
 //checkPhone function test
-console.log(lib.checkPhone(phone));
+console.log("phone test: " + lib.checkPhone(phone));
 
+//checkEmail function test
+console.log("email test: " + lib.checkEmail(email));
 alert("JavaScript works!");
--- clawson_nicholas_library.js
+++ clawson_nicholas_library.js
@@ -3,12 +3,12 @@ var nixLib = function (){
 	var checkPhone = function (phone){
 
 		/* RegExp below explained:
-		([0-9]{3}) checks that first 3 characters are digits 0-9, 
+		^([0-9]{3}) checks that first 3 characters are digits 0-9, 
 		[-]? checks for a dash but it is optional, 
 		([0-9]{3}) checks that next 3 characters are digits 0-9, 
 		[-]? checks for dash but is optional,
 		([0-9]{4})$ checks that the last 4 characters are digits 0-9*/
-		var pattern = new RegExp("([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$"); 
+		var pattern = new RegExp("^([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$"); 
 		if (pattern.test(phone)){
 
 			return true;
@@ -19,8 +19,28 @@ var nixLib = function (){
 		};
 	};
 
-	return {
-		"checkPhone": checkPhone
+	var checkEmail = function (email){
 
+		/* RegExp below explained:
+		^([a-zA-Z0-9_\.\-]) checks that the beginning the of string contains any letter, digit, underscore, period or dash. 
+		[@] checks for a literal @,
+		([a-zA-Z0-9]) checks the domain name for any letter or digit,
+		[\.] checks for a literal ., 
+		([a-zA-Z]{3})$ checks that the last 3 characters are any letter*/
+		var pattern = new RegExp("^([a-zA-Z0-9_\.\-])+[@]([a-zA-Z0-9])+[\.]([a-zA-Z]{3})$");
+		
+		if (pattern.test(email)){
+
+			return true;
+		}
+		else{
+
+			return false;
+		};
+	};
+
+	return {
+		"checkPhone": checkPhone,
+		"checkEmail": checkEmail
 	};
 };
