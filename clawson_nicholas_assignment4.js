1dfeb14bad5f146a9b5925a622abe7bc46150382
Nick Clawson <NickClawson@fullsail.edu>
2012-05-23 16:50:22 -0400

    created checkPhone function
    checkPhone in library file uses RegExp to check if a string follows a
    NANP phone number. returns true if it does, returns false if it does
    not. tested in main assignment file.

--- clawson_nicholas_assignment4.js
+++ clawson_nicholas_assignment4.js
@@ -1,1 +1,7 @@
+var phone = "440-506-8107";
+var lib = nixLib();
+
+//checkPhone function test
+console.log(lib.checkPhone(phone));
+
 alert("JavaScript works!");
--- clawson_nicholas_library.js
+++ clawson_nicholas_library.js
@@ -1,7 +1,26 @@
 var nixLib = function (){
 	
+	var checkPhone = function (phone){
 
+		/* RegExp below explained:
+		([0-9]{3}) checks that first 3 characters are digits 0-9, 
+		[-]? checks for a dash but it is optional, 
+		([0-9]{3}) checks that next 3 characters are digits 0-9, 
+		[-]? checks for dash but is optional,
+		([0-9]{4})$ checks that the last 4 characters are digits 0-9*/
+		var pattern = new RegExp("([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$"); 
+		if (pattern.test(phone)){
 
+			return true;
+		}
+		else{
+
+			return false;
+		};
 	};
 
+	return {
+		"checkPhone": checkPhone
+
+	};
 };
