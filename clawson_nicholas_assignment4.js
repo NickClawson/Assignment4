6dec08b1d45baafeda60d4bc3bca8a846a7054c3
Nick Clawson <NickClawson@fullsail.edu>
2012-05-23 21:16:24 -0400

    created checkURL function
    created checkURL function in library file to check the syntax of a
    given web address using regular expression. added a test for this
    function on the main assignment file.

--- clawson_nicholas_assignment4.js
+++ clawson_nicholas_assignment4.js
@@ -1,22 +1,30 @@
-var phone = "440-506-8107";
-var email = "NickClawson@fullsail.edu"
-var num = 5.14432645;
+//Author: Nick Clawson
+//Date: 5/23/12
+//SDI 1205
 
-var lib = nixLib();
+
+
+var phone = "440-506-8107"; //example phone number
+var email = "NickClawson@fullsail.edu" //example email
+var num = 5.1; //example number
+var url = "www.penny-arcade.com" //example URL
+var lib = nixLib(); //pulls library information into file
 
 //checkPhone function test
-console.log("phone test: " + lib.checkPhone(phone));
+console.log("phone test: " + lib.checkPhone(phone)); //displaying value of checkPhone test in console
 
 //checkEmail function test
-console.log("email test: " + lib.checkEmail(email));
+console.log("email test: " + lib.checkEmail(email)); //displaying value of checkEmail test in console
+
+//checkURL function test
+console.log("url test: " + lib.checkURL(url)); //displaying value of checkURL in console
 
 //roundDecimal function test
-console.log("rounding test: " + lib.roundDecimal(num, 2));
-
+console.log("rounding test: " + lib.roundDecimal(num, 2)); //displaying value of roundDecimal test in console
 
 //stringToNumber function test
 var numb = lib.stringToNumber("45.76") + 5; //performed basic arithmetic to determine if value was being returned properly
-console.log ("parse test: " + numb);
+console.log ("parse test: " + numb); // displaying value of above line in console
 
 
 
--- clawson_nicholas_library.js
+++ clawson_nicholas_library.js
@@ -1,14 +1,18 @@
-var nixLib = function (){
+//Author: Nick Clawson
+//Date: 5/23/12
+//SDI 1205
+
+var nixLib = function (){ //new library
 	
-	var checkPhone = function (phone){ //checks if a string matches a NANP phone number pattern
+	var checkPhone = function (phone){ //checks if a string matches NANP phone number syntax
 
 		/* RegExp below explained:
 		^([0-9]{3}) checks that first 3 characters are digits 0-9, 
-		[-]? checks for a dash but it is optional, 
+		[-]? checks for literal dash but it is optional, 
 		([0-9]{3}) checks that next 3 characters are digits 0-9, 
-		[-]? checks for dash but is optional,
+		[-]? checks for literal dash but is optional,
 		([0-9]{4})$ checks that the last 4 characters are digits 0-9*/
-		var pattern = new RegExp("^([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$"); 
+		var pattern = new RegExp("^([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$"); //explained above
 		
 		if (pattern.test(phone)){ //checking if arguement matches regular expression
 
@@ -20,15 +24,15 @@ var nixLib = function (){
 		};
 	};
 
-	var checkEmail = function (email){ //checks if a string matches a standard email address pattern
+	var checkEmail = function (email){ //checks if a string matches standard email address syntax
 
 		/* RegExp below explained:
 		^([a-zA-Z0-9_\.\-]) checks that the beginning the of string contains any letter, digit, underscore, period or dash. 
 		[@] checks for a literal @,
 		([a-zA-Z0-9]) checks the domain name for any letter or digit,
 		[\.] checks for a literal ., 
-		([a-zA-Z]{3})$ checks that the last 3 characters are any letter*/
-		var pattern = new RegExp("^([a-zA-Z0-9_\.\-])+[@]([a-zA-Z0-9])+[\.]([a-zA-Z]{3})$");
+		([a-zA-Z]{2,4})$ checks that the last 2-4 characters are any letter*/
+		var pattern = new RegExp("^([a-zA-Z0-9_\.\-])+[@]([a-zA-Z0-9])+[\.]([a-zA-Z]{2,4})$"); //explained above
 		
 		if (pattern.test(email)){ //checking if arguement matches regular expression
 
@@ -40,9 +44,31 @@ var nixLib = function (){
 		};
 	};
 
+	var checkURL = function (url){ //checking if a given URL matches standard web address syntax
+
+		/* RegExp below explained:
+		^([https?:\/\/])? checks if beginning of string begins with http:// or https://. it is optional. 
+		([www\.])? checks for "www." it is optional.
+		([a-zA-Z0-9\.\-]) checks site name for any alphanumerics, literal . and literal -
+		([a-zA-Z0-9]{2,4}) checks last 2 to 4 characters of domain name for any alphanumeric
+		([\/\?\$\&\=\_\.a-zA-Z0-9]?) checks for optional special characters and alphanumerics in web address following main domain name*/
+		var pattern = new RegExp("^([https?:\/\/])?([www\.])?([a-zA-Z0-9\.\-])([a-zA-Z0-9]{2,4})([\/\?\$\&\=\_\.a-zA-Z0-9]?)"); //explained above
+    	 if (pattern.test(url)){ //checking if arguement matches regular expression
+
+    	 	return true; //returns true if arguement matches regular expression
+    	 }
+    	 else{
+
+    	 	return false; //returns false if arguement does not match regualar expression
+    	 };
+
+	};
+
 	var roundDecimal = function (num, places){ //rounds a given number arguement to a given decimal place
 
-			return num.toFixed(places); //returns a number rounded to a given decimal place
+			var rounded = num.toFixed(places); //creates new var with value of arguement rounded to given decimal place
+
+			return rounded; //returns the arguement num rounded to the given decimal place
 
 	};
 
@@ -50,13 +76,14 @@ var nixLib = function (){
 
 		var num = parseFloat(sNum); //parseFloat returns a number from a string value regardless if it has a decimal or not. parseInt returns only integers
 
-		return num;
+		return num; //returns the number version of a stringified number
 	};
 
 	return {
-		"checkPhone": checkPhone,
-		"checkEmail": checkEmail,
-		"roundDecimal": roundDecimal,
-		"stringToNumber": stringToNumber
+		"checkPhone": checkPhone, //returns checkPhone function to be used outside of library
+		"checkEmail": checkEmail, //returns checkEmail function to be used outside of library
+		"checkURL": checkURL, //returns checkURL function to be used outside of library
+		"roundDecimal": roundDecimal, //returns roundDecimal function to be used outside of library
+		"stringToNumber": stringToNumber //returns stringToNumber function to be used outside of library
 	};
 };
